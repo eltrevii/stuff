9bf7c985169d06ee6ecfa12acafb4d55907ea018
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-02-22 19:14:07 +0100

    Update purpur-updater-plus.bat

--- purpur-updater-plus.bat
+++ purpur-updater-plus.bat
@@ -1,5 +1,6 @@
 @echo off
 title Purpur Updater Plus
+if not exist purpur\eula.txt (echo eula=true>eula.txt) 
 :1
 curl -k "https://aritz331.github.io/stuff/purpur-updater.bat" -o updater.bat --progress-bar
 start /wait /min "" cmd /c updater.bat
@@ -7,7 +8,7 @@ del updater.bat
 cls
 curl -k "https://aritz331.github.io/stuff/startpurpur.bat" -o start.bat --progress-bar
 start /min "" cmd /c start.bat
-timeout 3600 /nobreak >nul
+timeout 3600 /nobreak
 curl -k "https://aritz331.github.io/stuff/nssk.bat" --progress-bar -o nssk.bat
 cls
 start /wait /min "" cmd /c nssk.bat "Purpur Server" "stop"
