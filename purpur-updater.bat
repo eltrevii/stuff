66d205d1bf58ee08f0a91497c9f44555f7a05bc2
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-02-22 17:17:43 +0100

    Update purpur-updater.bat

--- purpur-updater.bat
+++ purpur-updater.bat
@@ -1,5 +1,7 @@
 @echo off
 md purpur
 cls
+echo downloading purpur 1.18.1-latest
+echo.
 curl -k "https://api.purpurmc.org/v2/purpur/1.18.1/latest/download" -L -o purpur\purpur.jar --progress-bar
 exit /b
