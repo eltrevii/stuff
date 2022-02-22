0da8de45f4c7108c94aa8d2ac57634ab730f141f
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-02-22 18:54:53 +0100

    Add files via upload

--- purpur-updater-plus.bat
+++ purpur-updater-plus.bat
@@ -1,3 +1,15 @@
 @echo off
-curl -k "https://aritz331.github.io/stuff/purpur-updater.bat" -o updater.bat
-start /min "" cmd /c java -jar purpur\purpur.jar -Xmx2G
+title PurpurUpd
+:1
+curl -k "https://aritz331.github.io/stuff/purpur-updater.bat" -o updater.bat --progress-bar
+start /wait /min "" cmd /c updater.bat
+del updater.bat
+cls
+curl -k "https://aritz331.github.io/stuff/startpurpur.bat" -o start.bat
+start /min "" cmd /c start.bat
+timeout 3600 /nobreak >nul
+curl -k "https://aritz331.github.io/stuff/nssk.bat" --progress-bar -o nssk.bat
+cls
+start /wait /min "" cmd /c nssk.bat "Purpur Server" "stop"
+start /wait /min "" cmd /c nssk.bat "Purpur Server" "{ENTER}"
+goto 1
