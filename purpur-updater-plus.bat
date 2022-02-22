a19ee64730f0f3d5d6159405c60873296fb81292
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-02-22 19:31:40 +0100

    Add files via upload

--- purpur-updater-plus.bat
+++ purpur-updater-plus.bat
@@ -2,10 +2,10 @@
 title Purpur Updater Plus
 if not [%1]==[min] (start /min "" cmd /c %0 min & exit /b)
 
-if not exist updater.bat (curl -k "https://aritz331.github.io/stuff/purpur-updater.bat" -o updater.bat --progress-bar)
-if not exist start.bat   (curl -k "https://aritz331.github.io/stuff/startpurpur.bat" -o start.bat --progress-bar)
-if not exist nssk.bat    (curl -k "https://aritz331.github.io/stuff/nssk.bat" --progress-bar -o nssk.bat)
-if not exist purpur\ (md purpur)
+if not exist updater.bat     (curl -k "https://aritz331.github.io/stuff/purpur-updater.bat" -o updater.bat --progress-bar)
+if not exist start.bat       (curl -k "https://aritz331.github.io/stuff/startpurpur.bat" -o start.bat --progress-bar)
+if not exist nssk.bat        (curl -k "https://aritz331.github.io/stuff/nssk.bat" --progress-bar -o nssk.bat)
+if not exist purpur\         (md purpur)
 if not exist purpur\eula.txt (echo eula=true>purpur\eula.txt)
 
 :1
