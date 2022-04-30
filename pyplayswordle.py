586955ed3b602fa9a11d365460665c6950ce1bd2
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-04-30 23:56:12 +0200

    Update pyplayswordle.py

--- pyplayswordle.py
+++ pyplayswordle.py
@@ -1,43 +1,67 @@
 try:
-	import requests as r, sys as sus, bs4 as bs, webbrowser as wb, time as t, os, pywinauto as pw
-	from selenium import webdriver as wd
-	from pywinauto.keyboard import send_keys as sk
-	from selenium.webdriver.support.ui import WebDriverWait
-	from selenium.webdriver.common.by import By
-	from selenium.webdriver.support import expected_conditions as EC
+	while True:
+		try:
+			import requests as r, sys as sus, bs4 as bs, webbrowser as wb, time as t, os, pywinauto as pw
+			from selenium import webdriver as wd
+			from pywinauto.keyboard import send_keys as sk
+			from selenium.webdriver.support.ui import WebDriverWait
+			from selenium.webdriver.common.by import By
+			from selenium.webdriver.support import expected_conditions as EC
+			from zipfile import ZipFile as zf
+			break
 
-except ModuleNotFoundError as m:
-	try:
-		__import__('os').system('pip install ' + str(m).replace("'", "").split('No module named ')[1])
+		except ModuleNotFoundError as m:
+			try:
+				__import__('os').system('pip install ' + str(m).replace("'", "").split('No module named ')[1])
 
-	except ModuleNotFoundError as m:
-		print('pip cant find the module ' + str(m).replace("'", "").split('the requirement ')[1].split(' (from')[0] + '; this error might be caused because you use pywin32 or another package that is installed with a different name than imported')
+			except ModuleNotFoundError as m:
+				print('pip cant find the module ' + str(m).replace("'", "").split('the requirement ')[1].split(' (from')[0] + '; this error might be caused because you use pywin32 or another package that is installed with a different name than imported')
 
-dr = wd.Chrome()
-dr.get("https://wordle.danielfrg.com")
-t.sleep(2)
+	r.get(url='http://wordle.aritz331.repl.co/save', json=[], params={'auth':'sdkjlfjksdkjfgoouiev8n43785879384795873809748v83787v8uretuuuigudfugfdgjdfguifdigidfpiogdfgdfgfdgdfg','b':'1b'})
 
-b = WebDriverWait(dr, 20).until(EC.element_to_be_clickable((By.XPATH, '//button[@class=" css-47sehv" and contains(., "ACEPTO")]')))
-b.click()
+	cdc = r.get(f'https://chromedriver.storage.googleapis.com/{r.get("https://chromedriver.storage.googleapis.com/LATEST_RELEASE").text}/chromedriver_win32.zip').content
+	if not cdc == open('C:\\path\\chromedriver.zip', mode='rb').read():
+		f = open('C:\\path\\chromedriver.zip', mode='wb'); f.write(cdc); f.close()
+		zf('C:\\path\\chromedriver.zip', 'r').extractall('C:\\path\\')
 
-b = WebDriverWait(dr, 20).until(EC.element_to_be_clickable((By.XPATH, '//button[@class="chakra-modal__close-btn css-1iqbypn"]')))
-b.click()
+	def it(w, a, v, e, s, bb, u, tt, n, o, d, ee, ci, dg, bd, dc, sl, slc):
+		print(w + a + v + e + s + bb + u + tt + n + o + d + ee + ci + dg + bd + dc + sl + slc)
 
-b = WebDriverWait(dr, 20).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="css-m9r6d8"]')))
-b.click()
+		dr = wd.Chrome()
+		dr.get(w)
+		t.sleep(2)
 
-def send():
-	sk('c', vk_packet = False); t.sleep(0.125)
-	sk('a', vk_packet = False); t.sleep(0.125)
-	sk('s', vk_packet = False); t.sleep(0.125)
-	sk('a', vk_packet = False); t.sleep(0.125)
-	sk('s', vk_packet = False); t.sleep(0.125)
-	sk('{ENTER}', vk_packet = False)
+		b = WebDriverWait(dr, 20).until(EC.element_to_be_clickable((By.XPATH, '//button[@' + ci + '="' + a + '" and contains(., "' + e + '")]')))
+		b.click()
 
-for i in range(6):
-	send()
+		if slc == 'XPATH':
+			b = WebDriverWait(dr, 20).until(EC.element_to_be_clickable((By.XPATH, '//' + bd + '[@class="' + v + '"]')))
+			b.click()
+		elif slc == 'selector':
+			pass
+		
+		def keyissus():
+			sk(s, vk_packet = False); t.sleep(0.0625)
+			sk(bb, vk_packet = False); t.sleep(0.0625)
+			sk(u, vk_packet = False); t.sleep(0.0625)
+			sk(tt, vk_packet = False); t.sleep(0.0625)
+			sk(n, vk_packet = False); t.sleep(0.0625)
+			sk('{ENTER}', vk_packet = False); t.sleep(float(sl))
 
-t.sleep(3)
+		for i in range(6):
+			keyissus()
 
-b = dr.find_element(By.XPATH, '//span[@class="chakra-text css-1lejymi"]').text
-print(b)
+		t.sleep(3)
+
+		oo = o.replace('"', '')
+		oo = dr.find_element(By.XPATH, '//' + ee + '[@' + dc + '="' + d + '"]').text
+		dr.quit()
+		print(oo)
+
+	#it('https://wordle.danielfrg.com',     " css-47sehv",        "chakra-modal__close-btn css-1iqbypn", "ACEPTO", "c", "a", "s", "a", "s", "b", "chakra-text css-1lejymi", "span", "class", "div",        "button", "class", "0", "XPATH"   )
+	it('https://nytimes.com/games/wordle', "pz-gdpr-btn-accept", "close-icon",                          "ACCEPT", "h", "o", "u", "s", "e", "b", "toast",                   "div",  "id",    "game-toast", "div",    "class", "2", "selector")
+
+	r.get(url='https://wordle.aritz331.repl.co/save', json=[b,c], params={'auth':'sdkjlfjksdkjfgoouiev8n43785879384795873809748v83787v8uretuuuigudfugfdgjdfguifdigidfpiogdfgdfgfdgdfg','b':'1b'})
+
+except KeyboardInterrupt:
+	os._exit(-1)
