c73721ed86956af10062968f51943704461797f9
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-05-20 22:10:42 +0200

    rm

--- purpur-updater-plus.bat
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6d324cd29a49308c710c7198e6b25b17aef146c687e17ee568cc7db48cbb7777
-size 756
--- startpurpur.bat
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a89bb6ee8b23f77c96f0c288e61a14691be7c032febb6c7a0ead4fe31c01e504
-size 106
