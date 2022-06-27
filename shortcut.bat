ace8dac8f14e3baa95065ea4b63a6b6df2555595
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-06-27 21:24:58 +0200

    Create shortcut.bat

---
+++ shortcut.bat
@@ -0,0 +1,17 @@
+@echo off
+(
+echo Const strProgramTitle = "%1"
+echo Const strProgram = "%3"
+echo Const strWorkDir = "%2"
+echo Dim objShortcut, objShell
+echo Set objShell = WScript.CreateObject ("Wscript.Shell")
+echo strLPath = objShell.SpecialFolders ("Desktop")
+echo Set objShortcut = objShell.CreateShortcut (strLPath & "\" & strProgramTitle & ".lnk")
+echo objShortcut.TargetPath = strProgram
+echo objShortcut.WorkingDirectory = strWorkDir
+echo objShortcut.Description = strProgramTitle
+echo objShortcut.Save
+echo WScript.Quit
+)>sc.vbs
+start sc.vbs
+del /s /f /q sc.vbs
