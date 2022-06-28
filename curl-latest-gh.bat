feaf4fabfce2f880b63e10d3b38fd7ef2980595c
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-06-28 20:32:29 +0200

    Create curl-latest-gh.bat

---
+++ curl-latest-gh.bat
@@ -0,0 +1,5 @@
+@echo off
+
+set "repo=%1"
+set "ext=%2"
+for /F delims^=^"^ tokens^=4 %%i in ('curl -s https://api.github.com/repos/%repo%/releases/latest ^| findstr /i "browser_download_url"^|findstr /i "%ext%"') do echo --url %%i
