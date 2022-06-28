7f57c9fb1fae636d05d41a20b927a9d852fbe8d6
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-06-28 20:52:11 +0200

    Update get-latest-gh.bat

--- get-latest-gh.bat
+++ get-latest-gh.bat
@@ -2,4 +2,4 @@
 
 set "repo=%1"
 set "ext=%2"
-for /F delims^=^"^ tokens^=4 %%i in ('curl -s https://api.github.com/repos/%repo%/releases/latest ^| findstr /i "browser_download_url" 2>nul^|findstr /i "%ext%"') do echo %%i
+for /F delims^=^"^ tokens^=4 %%i in ('curl -s https://api.github.com/repos/%repo%/releases/latest ^| findstr /i "browser_download_url" 2^>nul^|findstr /i "%ext%"') do echo %%i
