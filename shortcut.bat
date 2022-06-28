c1a84d34c7f9f35200f8bf07e8b526080de3d75a
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-06-28 20:14:35 +0200

    Update shortcut.bat

--- shortcut.bat
+++ shortcut.bat
@@ -4,9 +4,9 @@ echo Const strProgramTitle = %1
 echo Const strProgram = %3
 echo Const strWorkDir = %2
 echo Dim objShortcut, objShell
-echo Set objShell = WScript.CreateObject ("Wscript.Shell")
-echo strLPath = objShell.SpecialFolders ("Desktop")
-echo Set objShortcut = objShell.CreateShortcut (strLPath & "\" & strProgramTitle & ".lnk")
+echo Set objShell = WScript.CreateObject ("Wscript.Shell"^)
+echo strLPath = objShell.SpecialFolders ("Desktop"^)
+echo Set objShortcut = objShell.CreateShortcut (strLPath ^& "\" ^& strProgramTitle ^& ".lnk"^)
 echo objShortcut.TargetPath = strProgram
 echo objShortcut.WorkingDirectory = strWorkDir
 echo objShortcut.Description = strProgramTitle
